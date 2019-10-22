0880134378edaefadcb8057bc3db69b2b36d07ed
Shivangi Singh <shivangi.singh@insurancedekho.com>
2019-10-22 23:44:50 +0530

    structure api

---
+++ notis-backend/src/config/Utils.js
@@ -0,0 +1,59 @@
+class Utils{
+    /**
+     * returns true if data is '' or null or undefined
+     * for object, checks if it's length is non zero or not
+     * @param data {Mixed}
+     * @param zeroIsNotEmpty {boolean}: pass true only if you want 0 to be considered as non-empty
+     * @returns {boolean}
+     */
+    isEmpty(data, zeroIsNotEmpty) {
+        if(typeof data !== 'object' && (data === null || data === '' || typeof data === 'undefined')) {
+            return true;
+        } else if(data === null) {
+            return true;
+        } else if(typeof data === 'string' && data === '0' && !zeroIsNotEmpty) {
+            return true;
+        } else if(typeof data.length !== 'undefined') {
+            if(data.length > 0) {
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            if(Object.keys(data).length > 0) {
+                return false;
+            } else if(typeof data === 'number' && (data !== 0 || zeroIsNotEmpty)) {
+                return false;
+            } else {
+                if(data === true) {
+                    return false
+                }
+                return true;
+            }
+        }
+    }
+
+    convertIn2Digits(d){
+        d = d+"";
+        if(d.length < 2){
+            return "0"+d
+        }else{
+            return d
+        }
+    }
+
+    get6DigitRandNo(){
+        return Math.floor(100000 + Math.random() * 900000)
+    }
+
+    getNOnce(){
+        let d = new Date();
+        let buff = new Buffer(`${d.getFullYear()}${this.convertIn2Digits(d.getMonth()+1)}${this.convertIn2Digits(d.getDate())}${this.utils.convertIn2Digits(d.getHours())}${this.convertIn2Digits(d.getMinutes())}${this.utils.convertIn2Digits(d.getSeconds())}${this.convertIn2Digits(d.getMilliseconds())}:${this.get6DigitRandNo()}`);
+        let base64data = buff.toString('base64');
+    
+        // console.log('"' + data + '" converted to Base64 is "' + base64data + '"');
+        return base64data;
+    }
+}
+
+module.exports = Utils;
--- notis-backend/src/routes/nseRoutes.js
+++ notis-backend/src/routes/nseRoutes.js
@@ -1,5 +1,5 @@
-let NseController = require("../app/nse/controllers/nseToken");
-
+let NseToken = require("../app/nse/controllers/nseToken");
+let nseToken = new NseToken();
 
 
 module.exports = function(app, router) {
@@ -10,6 +10,23 @@ module.exports = function(app, router) {
         });
     });
 
-    app.get('/token', NseController.getToken);
-    app.get('/dbConnection', NseController.getDbData);
+    app.get('/token', nseToken.getToken);
+    app.get('/dbConnection', nseToken.getDbData);
+    app.get('/getNseUtils', async function(req, res){
+        let NseUtils = require("../app/nse/controllers/nseUtils");
+        let nseUtils = new NseUtils();
+        res.send(await nseUtils.getNseData("ab", "cd"));
+
+    });
+
+    app.get('/getNotis-cd', async function(req, res){
+        let NseUtils = require("../app/nse/controllers/nseUtils");
+        let nseUtils = new NseUtils();
+        let trade_type = req.query.trade_type;
+        try{ 
+            res.send(await nseUtils.getNseData([trade_type]));
+        } catch(e){
+            res.send({err: e});
+        }
+    });
 }
