18159d9ec7c5492da8a18a4c18b98704ab0bc401
Vishal <visha.1@girnarsoft.com>
2019-12-28 01:40:08 +0530

    Basic Info card added in profile section

--- notis-frontend/src/app/profile/profile.component.ts
+++ notis-frontend/src/app/profile/profile.component.ts
@@ -8,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class ProfileComponent implements OnInit {
 
   constructor() { }
-
+  username : 	String = "anand001";
+  firstname: 	String = "Anand";
+  lastname:  	String = "Modi";
+  empcode: 		String = "empc001";
+  user_occupation: 	String = "COO";
+  user_email: 		String = "anand001@wilsone.com";
+  user_phone: 		String = "+91 9876543210";
+  user_role: 		String = "admin";
+  last_login_at: 	Date = new Date(); 
   ngOnInit() {
   }
 
